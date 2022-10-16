b0a06aca8ee27869d3e1b24093a2c85131fb2d98
MrSimbax <matt@przybyl.io>
2022-10-16 19:26:05 +0200

    release

---
+++ make_package.sh
@@ -0,0 +1,12 @@
+version="1.1.0"
+current=`pwd`
+path="bin/x64/plugins/cyber_engine_tweaks/mods/wardrobe_items_adder"
+package_name="wardrobe_items_adder_v${version}"
+package_path="${package_name}/${path}"
+cd ..
+mkdir -p "${package_path}"
+cd ${current}
+find . -iname '*.lua' -type f -exec cp --parents {} "../${package_path}" \;
+cd "../${package_name}"
+rm "${path}/config.lua"
+zip -r "../${package_name}.zip" .
